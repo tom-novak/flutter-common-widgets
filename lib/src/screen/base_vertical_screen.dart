9ea6f9fd341f671a19fc46ce020a9e9c1eb66047
Tomáš Novák <hello@tom-novak.com>
2021-11-29 13:54:54 +0100

    Common list page

--- lib/src/screen/base_vertical_screen.dart
+++ lib/src/screen/base_vertical_screen.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_common_widgets/flutter_common_widgets.dart';
-import 'package:flutter_common_widgets/src/widget/titled_message.dart';
-import 'package:flutter_conditional_rendering/conditional_switch.dart';
-
-String contentStateBuilder(BuildContext context) => WidgetState.content;
 
 class BaseVerticalScreen extends StatelessWidget {
   final PreferredSizeWidget? appBar;
   final Widget? appBarTitle;
-  final Widget? aboveContent;
-  final Widget? content;
-  final Widget? belowContent;
+  final Widget? body;
   final Widget? bottomNavigationBar;
   final String Function(BuildContext) widgetStateBuilder;
 
@@ -18,9 +12,7 @@ class BaseVerticalScreen extends StatelessWidget {
     Key? key,
     this.appBar,
     this.appBarTitle,
-    this.aboveContent,
-    this.content,
-    this.belowContent,
+    this.body,
     this.bottomNavigationBar,
     String Function(BuildContext)? widgetStateBuilder,
   }) : widgetStateBuilder = widgetStateBuilder ?? contentStateBuilder,
@@ -33,35 +25,7 @@ class BaseVerticalScreen extends StatelessWidget {
           AppBar(
             title: appBarTitle,
           ),
-      body: ConditionalSwitch.single<String>(
-        context: context,
-        valueBuilder: widgetStateBuilder,
-        caseBuilders: {
-          WidgetState.content: (context) {
-            return Column(
-              children: [
-                if (aboveContent != null)
-                  Container(
-                    child: aboveContent,
-                  ),
-                Expanded(
-                  child: content ?? const SizedBox.shrink(),
-                ),
-                if (belowContent != null)
-                  Container(
-                    child: belowContent,
-                  ),
-              ],
-            );
-          },
-          WidgetState.loading: (context) => const ShimmerList(itemCount: 7,),
-          WidgetState.error: (context) => const TitledMessage(
-                title: 'Error',
-                message: 'Something wrong',
-              )
-        },
-        fallbackBuilder: (context) => const SizedBox.shrink(),
-      ),
+      body: body,
       bottomNavigationBar: bottomNavigationBar,
     );
   }
---
+++ lib/src/screen/export.dart
@@ -0,0 +1,3 @@
+export 'base_vertical_page.dart';
+export 'base_vertical_screen.dart';
+export 'main_screen.dart';
