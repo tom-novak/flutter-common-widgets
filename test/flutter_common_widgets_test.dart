cf67d4a18dee371d386fec41f9d811dc7b729d63
Tomáš Novák <hello@tom-novak.com>
2022-01-04 13:14:51 +0100

    Android build added to GitHub action

--- test/flutter_common_widgets_test.dart
+++ test/flutter_common_widgets_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_common_widgets/flutter_common_widgets.dart';
-
 void main() {
   test('adds one to input values', () {
   });
