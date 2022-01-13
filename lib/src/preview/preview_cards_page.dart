6b8835771cc5e2d75cc10ad1a837cab4d3cdac6d
Tomáš Novák <hello@tom-novak.com>
2022-01-13 16:33:52 +0100

    Merge issues solved

--- lib/src/preview/preview_cards_page.dart
+++ lib/src/preview/preview_cards_page.dart
@@ -42,7 +42,7 @@ class _PreviewCardsPageState extends State<PreviewCardsPage> {
                     MaterialPageRoute(
                       builder: (context) {
                         return const BaseVerticalScreen(
-                          body: PreviewDetailPage(),
+                          body: PreviewDetailPage(itemId: 0,),
                         );
                       },
                     ),
